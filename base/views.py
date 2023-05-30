1960e9f8abda6dc1416fe21fe62f8942b3b39d0c
Ludovic Dumas <ludoloops@pm.me>
2023-05-30 18:04:31 -0400

    feat: mobile view + search and last activities

--- base/views.py
+++ base/views.py
@@ -81,7 +81,8 @@ def home(request):
         Q(name__icontains=q) |
         Q(description__icontains=q)
     )
-    topics = Topic.objects.all()
+    # [0:5] to get only the item from 0 to before 5
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
@@ -207,3 +208,14 @@ def updateUser(request):
             return redirect('user-profile', pk=user.id)
 
     return render(request, 'base/update-user.html', {'form': form})
+
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, 'base/topics.html', {'topics': topics})
+
+
+def activitiesPage(request):
+    room_messages = Message.objects.all()
+    return render(request, 'base/activity.html', {'room_messages': room_messages})
